45edbf40cfa6e432061faf9ce30dbf7007bc0fe6
fuzzybear3965 <johnrichardrinehart@gmail.com>
2017-09-10 21:05:22 -0400

    all working

--- src/fft_with_units.jl
+++ src/fft_with_units.jl
@@ -26,7 +26,27 @@ end
 
 Returns a tuple of time-domain points with associated frequencies.
 
-Assumes unit of argument `x` is rad/sec.
+Assumes unit of argument `x` is Hertz (not rad/sec).
 """
-function ifft_with_units(xdata, ydata)
+function ifft_with_units(x, y)
+    if length(x) !== length(y)
+        error("length(x) !== length(y)")
+    end
+    out = ifft(y)
+    deltaf = x[2]-x[1]
+    N = Int(length(x))
+    if N%2 == 0
+        delta_t = 1/(2*(x[Int(.5*N)]+deltaf))
+    else
+        delta_t = 1/(2*(x[Int(.5*(N+1))])+deltaf)
+    end
+    tmax = 1/deltaf - delta_t # max time
+    ts = 0:delta_t:tmax
+    if length(ts) !== length(x)
+        @printf("length(ts): %d, length(x): %d, delta_t: %f, tmax: %f.\n",
+                length(ts), length(x), delta_t, tmax)
+        error("Number of time points should match the number of frequency
+              points.")
+    end
+    return (ts, out)
 end
